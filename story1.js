9dfa29f6ffe01d29be87f1fd7a55283f0ef7f06f
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-05-09 23:33:46 +0530

    Added authController

---
+++ story1.js
@@ -0,0 +1,40 @@
+/**
+ * We need to go and create APIs for Authentication and Authorisation
+ * 
+ *    
+ *     POST : /jobhunter/api/v1/signup
+ * 
+ * 
+ * 
+ *     POST : /jobhunter/api/v1/signin    --- Login
+ *     
+ * 
+ * 
+ *     Return me the JWT Access Token
+ * 
+ *  1. Fork 
+ *  2. Pull the lastest Code from Vishwa Main Branch
+ *  3. Create a new LocalBranch
+ *  4. Make the changest to support Authentication and Authorization
+ *  5. Push to your Repos
+ *  6. Raise the PR.
+ * 
+ *   
+ *   To get the latest Code from Vishwa repo
+ * 
+ *   ****
+ * 
+ *    1. Need to add vishwa Repo to your  Local CodeBase
+ *      --- git remote add upstream <vishwa code repo >
+ * 
+ *      -- git fetch upstream // this will get you latest change done by vishwa
+ * 
+ *  2. Create a new Branch based on Upstream/main
+ *     -- git checkout -b <branchName> upstream/main
+ * 
+ *    ------- make changes
+ * 
+ *     -- git push -u origin <branchName>
+ *  
+ *     .. Raise the PR from Github 
+ */
